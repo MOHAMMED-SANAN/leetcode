216b7c39e384c8dc9fd5838524be09346efd558d
MOHAMMED-SANAN <mmohammedsanan@gmail.com>
2026-02-04 10:29:56 +0530

    Time: 270 ms (67.02%), Space: 66.9 MB (47.05%) - LeetHub

---
+++ 2882-drop-duplicate-rows/2882-drop-duplicate-rows.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    customers = customers.drop_duplicates(subset=['email'])
+    return customers
+
+   
+    
